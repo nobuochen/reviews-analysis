ff959fafa5e09a5a29818baae96946eeb9703552
gary chen <nobuosan42@gmail.com>
2020-08-11 01:08:01 +0800

    add filter function

--- read.py
+++ read.py
@@ -11,3 +11,11 @@ sum = 0
 for d in data:
 	sum = sum + len(d)
 print('平均留言長度為', sum/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+	    new.append(d)
+print('一共有', len(new), '筆留言	長度小於100')
+print(new[0])
+print(new[1])
